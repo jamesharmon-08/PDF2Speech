b2985c1de93e81105ec81deea34e6cce9ef4719a
jamesharmon <james.harmon@outlook.com>
2021-07-30 18:54:36 -0400

    Error exception for Windows users.

--- speech.py
+++ speech.py
@@ -6,7 +6,10 @@ class Speech():
         super().__init__()
         self.engine = pyttsx3.init()
         self.voices = self.engine.getProperty('voices')
-        self.engine.setProperty('voice', 'com.apple.speech.synthesis.voice.karen')  # woman's voice
+        try:
+            self.engine.setProperty('voice', 'com.apple.speech.synthesis.voice.karen')  # woman's voice
+        except: # this will
+            self.engine.setProperty('voice', self.voices[1].id)  # incase it is run on Windows computer.
         # self.engine.setProperty('rate', 205)
 
     def say(self, text):
